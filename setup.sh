5805f649df6d9061321a77b7b6fb95bbc236d75a
dambergn <dambergn@gmail.com>
2019-08-12 18:17:41 -0700

    updated setup.sh to install node if not installed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,33 @@
 #/bin/bash
 
-echo "setting npm user home directory"
-sudo chown -R $USER:$(id -gn $USER) $HOME/.config
+# Setting user permissions
+echo "Setting npm user home directory"
+#sudo chown -R $USER:$(id -gn $USER) $HOME/.config
 
-echo "installing NPM packages"
-npm install
+# Check if node is installed
+echo "Checking software dependencies"
+if ! [ -x "$(command -v node)" ]; then
+  echo 'Error: node is not installed.' >&2
+  
+  echo 'Pulling updates'
+  sudo apt-get update
+
+  echo 'Installing CURL'
+  sudo apt-get install curl
+
+  echo 'Downloading packages'
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+  echo 'Installing Node and NPM'
+  sudo apt-get install -y nodejs
+
+  echo 'Installing common npm packages'
+  sudo npm install -g nodemon live-server -y
+
+  echo 'Node installation complete'
+
+fi
+
+# Installing node_modules
+echo "Installing NPM packages"
+#npm install
